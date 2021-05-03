411dd4ad6df1bd3b061447e58720b6827281627a
Ori Rawlings <orirawlings@gmail.com>
2021-05-03 17:34:02 -0500

    demo-02-kind.sh: Simplify

--- demo-02-kind.sh
+++ demo-02-kind.sh
@@ -5,12 +5,4 @@ cd $(dirname $0)
 set -x
 
 clear
-which go docker
-read && clear
-GO111MODULE="on" go get sigs.k8s.io/kind@v0.10.0
-read && clear
 kind create cluster
-read
-kind load docker-image container-example:latest
-read
-kind load docker-image gcr.io/kuar-demo/kuard-amd64:blue
