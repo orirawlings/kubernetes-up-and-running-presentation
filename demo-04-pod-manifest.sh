bd2a41230cd441c22fc5fb86c1671bcac65218f9
Ori Rawlings <orirawlings@gmail.com>
2021-05-05 14:22:20 -0500

    Add demo for creating pod from manifest file

---
+++ demo-04-pod-manifest.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+cd $(dirname $0)
+
+set -x
+
+# Create a pod via a manifest file
+clear
+cat pod/kuard.yaml
+read
+kubectl apply -f pod/kuard.yaml
+kubectl get pods -w
+read
+echo 'Run `kubectl port-forward kuard 8080:8080` and open http://localhost:8080/'
+read
+
+# Delete the pod
+clear
+kubectl delete pod kuard
+kubectl get pods
+
