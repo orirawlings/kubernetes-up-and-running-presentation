a9f84de55f88c56ab0b020698cb3df7cc4e1a8a3
Ori Rawlings <orirawlings@gmail.com>
2021-05-05 14:21:55 -0500

    Add comments to demo scripts

--- demo-01-container-example.sh
+++ demo-01-container-example.sh
@@ -4,13 +4,19 @@ cd $(dirname $0)
 
 set -x
 
+# Show container manifest and content
 clear
 cat container-example/Dockerfile
 read
 cat container-example/my-script.sh
 read
+
+# Build the container image with Docker
 docker build -t container-example:latest container-example/
-read && clear
+read
+
+# Show built image and create a new container with it
+clear
 docker images container-example
 read && clear
 docker run container-example:latest
--- demo-02-kind.sh
+++ demo-02-kind.sh
@@ -4,5 +4,6 @@ cd $(dirname $0)
 
 set -x
 
+# Create a new, local, single-node Kubernetes cluster
 clear
 kind create cluster
